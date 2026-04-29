2be247a96762a10e9d58cd9634acbdcb59088409
PavanSakthivel <sakpavan@gmail.com>
2026-04-29 10:20:31 +0530

    UC2 Added

--- test/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ test/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -1,10 +1,12 @@
 package com.apps.quantitymeasurement;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
 import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Inches;
 
 public class QuantityMeasurementAppTest {
+
+    // ----------- FEET TESTS -----------
 
     @Test
     public void testFeetEquality_SameValue() {
@@ -42,5 +44,45 @@ public class QuantityMeasurementAppTest {
         Feet f1 = new Feet(5.0);
 
         assertTrue(f1.equals(f1));
+    }
+
+    // ----------- INCHES TESTS -----------
+
+    @Test
+    public void testInchesEquality_SameValue() {
+        Inches i1 = new Inches(12.0);
+        Inches i2 = new Inches(12.0);
+
+        assertTrue(i1.equals(i2));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue() {
+        Inches i1 = new Inches(12.0);
+        Inches i2 = new Inches(24.0);
+
+        assertFalse(i1.equals(i2));
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison() {
+        Inches i1 = new Inches(12.0);
+
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentClass() {
+        Inches i1 = new Inches(12.0);
+        Feet notInches = new Feet(1.0);
+
+        assertFalse(i1.equals(notInches));
+    }
+
+    @Test
+    public void testInchesEquality_SameReference() {
+        Inches i1 = new Inches(12.0);
+
+        assertTrue(i1.equals(i1));
     }
 }
